25183bbc5a863b7daacf0ffbc68e5a5dc8e0a896
Dave Brackman <DBrackman@gmail.com>
2018-04-15 11:33:03 -0400

    Support a longer config-file line-length.  [Not sure how this was working in the past, because I found that our ~130-char config line (for project 4) was corrupting the IP address for project 1.]

--- libraries/BuildLightConfigurationManager/config.h
+++ libraries/BuildLightConfigurationManager/config.h
@@ -1,7 +1,7 @@
 #ifndef BuildLightConfigurationManager_config_h
 #define BuildLightConfigurationManager_config_h
 
-#define MAX_CONFIG_LINE_LENGTH 100
+#define MAX_CONFIG_LINE_LENGTH 150
 #define MAX_LOCATIONS_PER_LINE 4
 #define MAX_SUPPORTED_JOBS 5
 #define JENKINS_POST_JOB_URL "/api/json?tree=color"
